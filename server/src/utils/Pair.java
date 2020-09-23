619891ddb35f7b0b61eaa9f93930d539da37c493
Hugo <hugo.bollon@gmail.com>
2020-09-23 08:14:12 +0200

    Added Pair data structure util class

---
+++ server/src/utils/Pair.java
@@ -0,0 +1,49 @@
+package utils;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Custom Pair class who implements serialization
+ * Use for data transfer between devices (Game instance, Object)
+ * @param <F> -> Game
+ * @param <S> -> Object
+ */
+
+public class Pair<F, S> implements Serializable {
+    public final F first;
+    public final S second;
+
+    public Pair(F s, S o)
+    {
+        this.first = s;
+        this.second = o;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> that = (Pair<?, ?>) o;
+        return first.equals(that.first) &&
+                second.equals(that.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+
+    public static <A, B> Pair<A, B> create(A a, B b){
+        return new Pair<A, B>(a, b);
+    }
+
+}
