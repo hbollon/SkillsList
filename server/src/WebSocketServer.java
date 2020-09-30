b7f86496ed991c1a7262acfe5eaf5bd13d636f1f
Hugo <hugo.bollon@gmail.com>
2020-09-30 10:44:44 +0200

    Imported websocket lib + Api websocket class

---
+++ server/src/WebSocketServer.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/api")
+public class WebSocketServer {
+    @OnOpen
+    public void onOpen(Session s){
+        System.out.println("Websocket opened!");
+    }
+
+    @OnMessage
+    public void onMessage(String msg, Session s) throws IOException {
+        System.out.println("Message: " + msg);
+        s.getBasicRemote().sendText(msg);
+    }
+
+    @OnClose
+    public void onClose(CloseReason reason, Session s) {
+        System.out.println("Websocket closed!");
+    }
+
+    @OnError
+    public void onError(Session s, Throwable err) {
+        err.printStackTrace();
+    }
+
+}
