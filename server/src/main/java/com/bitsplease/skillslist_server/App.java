eb66f1a744cc1390879f992cc34c59d5361bb25d
Hugo <hugo.bollon@gmail.com>
2020-10-30 19:29:12 +0100

    Working HelloWorld SpringBoot Server

--- server/src/main/java/com/bitsplease/skillslist_server/App.java
+++
@@ -1,8 +0,0 @@
-import database.DatabaseHandler;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        DatabaseHandler db = new DatabaseHandler();
-
-    }
-}
--- server/src/main/java/com/bitsplease/skillslist_server/SkillslistServerApplication.java
+++ server/src/main/java/com/bitsplease/skillslist_server/SkillslistServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkillslistServerApplication {
 
 	public static void main(String[] args) {
+		database.DatabaseHandler db = new database.DatabaseHandler();
 		SpringApplication.run(SkillslistServerApplication.class, args);
 	}
 
