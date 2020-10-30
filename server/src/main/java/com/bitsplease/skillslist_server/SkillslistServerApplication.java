d5f1875936d2dee84fd1cc56d63644d543f3e8f1
Hugo <hugo.bollon@gmail.com>
2020-10-30 18:31:56 +0100

    Spring framework implementation & Project structure refactor

---
+++ server/src/main/java/com/bitsplease/skillslist_server/SkillslistServerApplication.java
@@ -0,0 +1,13 @@
+package com.bitsplease.skillslist_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillslistServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillslistServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/bitsplease/skillslist_server/SkillslistServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitsplease.skillslist_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillslistServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
